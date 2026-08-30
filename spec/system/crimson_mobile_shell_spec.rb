c0bd9ccc0fc79a6bfaea7c7dcd8836c4027703da
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 00:18:30 +0300

    STYLE: apply Syntax Tree formatting

--- spec/system/crimson_mobile_shell_spec.rb
+++ spec/system/crimson_mobile_shell_spec.rb
@@ -19,17 +19,11 @@ RSpec.describe "Crimson Channels mobile shell", mobile: true do
     toggle.click
 
     expect(page).to have_css("body.cn-mobile-community-open", visible: :all)
-    expect(page).to have_css(
-      ".cn-mobile-community-toggle[aria-expanded='true']",
-      visible: true,
-    )
+    expect(page).to have_css(".cn-mobile-community-toggle[aria-expanded='true']", visible: true)
 
     find("[data-cn-action='close-mobile-community']", visible: :all).click
 
     expect(page).to have_no_css("body.cn-mobile-community-open", visible: :all)
-    expect(page).to have_css(
-      ".cn-mobile-community-toggle[aria-expanded='false']",
-      visible: true,
-    )
+    expect(page).to have_css(".cn-mobile-community-toggle[aria-expanded='false']", visible: true)
   end
 end
