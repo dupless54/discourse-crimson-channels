235e9c058e5af9a36f2cfd461dff6c965b9c9e5f
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 00:13:24 +0300

    TEST: cover group-restricted navigation

--- spec/system/crimson_shell_spec.rb
+++ spec/system/crimson_shell_spec.rb
@@ -38,4 +38,52 @@ RSpec.describe "Crimson Channels shell" do
     expect(page).to have_css(".cn-server-rail")
     expect(page).to have_no_css(".cn-server-rail a.cn-server-button")
   end
+
+  it "shows group-restricted navigation only to matching members" do
+    group = Fabricate(:group)
+    member = Fabricate(:user)
+    group.add(member)
+
+    theme.update_setting(
+      :navigation_items,
+      [
+        {
+          enabled: true,
+          label: "Public",
+          url: "/",
+          icon: "house",
+          style: "brand",
+          visibility: "everyone",
+          mobile_shortcut: false,
+        },
+        {
+          enabled: true,
+          label: "Members",
+          url: "/latest",
+          icon: "lock",
+          style: "standard",
+          visibility: "groups",
+          groups: [group.id],
+          mobile_shortcut: false,
+        },
+      ],
+    )
+    theme.save!
+
+    visit("/")
+
+    expect(page).to have_css(
+      ".cn-server-rail a[data-cn-object-navigation='true'][data-label='Public']",
+    )
+    expect(page).to have_no_css(
+      ".cn-server-rail a[data-cn-object-navigation='true'][data-label='Members']",
+    )
+
+    sign_in(member)
+    visit("/")
+
+    expect(page).to have_css(
+      ".cn-server-rail a[data-cn-object-navigation='true'][data-label='Members'][href='/latest']",
+    )
+  end
 end
